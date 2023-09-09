18b92828f6e0f3ddcd525e53ba20fe7c3d024224
coding-chance <90079117+coding-chance@users.noreply.github.com>
2023-09-09 22:56:02 +0200

    Deleted test files

--- pykakasi-test.py
+++
@@ -1,7 +0,0 @@
-import pykakasi
-kks = pykakasi.kakasi()
-text = "明後日"
-result = kks.convert(text)
-print(result)
-# for item in result:
-#     print("{}: kana '{}', hiragana '{}', romaji: '{}'".format(item['orig'], item['kana'], item['hira'], item['hepburn']))
